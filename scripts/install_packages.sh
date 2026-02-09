436b050083c1c2ffc0ef6acfb0967dc22893329b
xtreamx2 <103985527+xtreamx2@users.noreply.github.com>
2026-02-09 10:57:45 +0100

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,8 +5,8 @@ echo "[install_packages] Instaluję pakiety systemowe..."
 
 sudo apt install -y \
     mpd mpc \
-    bluez bluealsa bluealsa-aplay \
-    camilladsp \
+#    bluez bluealsa bluealsa-aplay \
+#    camilladsp \
     python3 python3-pip python3-venv \
     git i2c-tools alsa-utils \
     curl wget unzip
