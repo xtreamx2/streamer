481ad0645621726c1b09d6c7a6cbad7dd8092133
xtreamx2 <103985527+xtreamx2@users.noreply.github.com>
2026-02-10 14:06:40 +0100

    Update install_python.sh

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "[install_python] Instaluję biblioteki Python..."
 
-sudo apt update
 sudo apt install -y python3-pip python3-venv build-essential libjpeg-dev zlib1g-dev
 
 sudo -H python3 -m pip install --break-system-packages --upgrade pip setuptools wheel
@@ -14,3 +13,5 @@ sudo -H python3 -m pip install --break-system-packages \
     pillow \
     python-mpd2 \
     luma.oled
+    
+sudo apt install -y python3-flask
